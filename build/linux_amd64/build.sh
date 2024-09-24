4406cc425a29359f267a773d527d39cd3e650883
xuges <xuges@qq.com>
2024-09-24 09:31:40 +0800

    增加pdfium-worker在linux_amd64平台构建方法。Add pdfium-worker build script for linux amd64 platform.

---
+++ build/linux_amd64/build.sh
@@ -0,0 +1,16 @@
+echo prefix=$(pwd)/pdfium > pdfium.pc
+cat >> pdfium.pc << EOF
+includedir=\${prefix}/include
+libdir=\${prefix}/lib
+Name: PDFium
+Description: PDFium
+Version: 6392
+Requires:
+Libs: -L\${libdir} -lpdfium
+Cflags: -I\${includedir}
+EOF
+
+export CGO_ENABLED=1
+export CGO_LDFLAGS='-Wl,-rpath=$ORIGIN'
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$(pwd)
+go build -tags pdfium_experimental -o pdfium-worker worker.go
