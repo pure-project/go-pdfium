f365ae189dc6a9a5686fb076e7a62b94f08446cd
xuges <xuges@qq.com>
2024-11-04 17:17:42 +0800

    递归提取FORM里的图片。GetPageImage supports recursive Form.

--- internal/implementation_cgo/graphs.go
+++ internal/implementation_cgo/graphs.go
@@ -30,7 +30,7 @@ func (p *PdfiumImplementation) GetPageImage(request *requests.GetPageImage) (*re
 	for i := 0; i < int(objCount); i++ {
 		obj := C.FPDFPage_GetObject(pageHandle.handle, C.int(i))
 		objType := enums.FPDF_PAGEOBJ(int(C.FPDFPageObj_GetType(obj)))
-		if objType == enums.FPDF_PAGEOBJ_IMAGE || objType == enums.FPDF_PAGEOBJ_FORM {
+		if objType == enums.FPDF_PAGEOBJ_IMAGE {
 			var left, bottom, right, top C.float
 			success := C.FPDFPageObj_GetBounds(obj, &left, &bottom, &right, &top)
 			if int(success) == 1 {
@@ -43,7 +43,7 @@ func (p *PdfiumImplementation) GetPageImage(request *requests.GetPageImage) (*re
 					},
 				}
 
-				if objType == enums.FPDF_PAGEOBJ_IMAGE && request.Bitmap {
+				if request.Bitmap {
 					bitmap := C.FPDFImageObj_GetBitmap(obj)
 					format := C.FPDFBitmap_GetFormat(bitmap)
 					if enums.FPDF_BITMAP_FORMAT(format) != enums.FPDF_BITMAP_FORMAT_UNKNOWN {
@@ -68,6 +68,8 @@ func (p *PdfiumImplementation) GetPageImage(request *requests.GetPageImage) (*re
 
 				images = append(images, image)
 			}
+		} else if objType == enums.FPDF_PAGEOBJ_FORM {
+			images = getFormImage(obj, request.Bitmap, images)
 		}
 	}
 
@@ -75,6 +77,57 @@ func (p *PdfiumImplementation) GetPageImage(request *requests.GetPageImage) (*re
 		Page:   pageHandle.index,
 		Images: images,
 	}, nil
+}
+
+func getFormImage(formObj C.FPDF_PAGEOBJECT, decode bool, images []responses.GetPageImageData) []responses.GetPageImageData {
+	fObjCount := C.FPDFFormObj_CountObjects(formObj)
+	for i := 0; i < int(fObjCount); i++ {
+		fObj := C.FPDFFormObj_GetObject(formObj, C.ulong(i))
+		fObjType := enums.FPDF_PAGEOBJ(int(C.FPDFPageObj_GetType(fObj)))
+		if fObjType == enums.FPDF_PAGEOBJ_IMAGE {
+			var left, bottom, right, top C.float
+			success := C.FPDFPageObj_GetBounds(fObj, &left, &bottom, &right, &top)
+			if int(success) == 1 {
+				image := responses.GetPageImageData{
+					Position: responses.ImagePosition{
+						Left:   float64(left),
+						Bottom: float64(bottom),
+						Right:  float64(right),
+						Top:    float64(top),
+					},
+				}
+
+				if decode {
+					bitmap := C.FPDFImageObj_GetBitmap(fObj)
+					format := C.FPDFBitmap_GetFormat(bitmap)
+					if enums.FPDF_BITMAP_FORMAT(format) != enums.FPDF_BITMAP_FORMAT_UNKNOWN {
+						width := C.FPDFBitmap_GetWidth(bitmap)
+						height := C.FPDFBitmap_GetHeight(bitmap)
+						stride := C.FPDFBitmap_GetStride(bitmap)
+						buffer := C.FPDFBitmap_GetBuffer(bitmap)
+
+						length := stride * height
+						buf := make([]byte, int(length))
+						C.memcpy(unsafe.Pointer(&buf[0]), buffer, C.size_t(length))
+
+						image.Bitmap = responses.ImageBitmap{
+							Format: enums.FPDF_BITMAP_FORMAT(format),
+							Stride: int(stride),
+							Width:  int(width),
+							Height: int(height),
+							Pix:    buf,
+						}
+					}
+				}
+
+				images = append(images, image)
+			}
+		} else if fObjType == enums.FPDF_PAGEOBJ_FORM {
+			images = getFormImage(fObj, decode, images)
+		}
+	}
+
+	return images
 }
 
 // GetPagePath returns all the path of a page
